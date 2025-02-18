0f4e3bc20689c96d60f87c1b02923feeec937886
Jenno20 <jnoerbjerg@gmail.com>
2025-02-18 12:42:42 +0100

    2nd draft of Invoke_Ubuntu_WinDC and initail commit of CreateUser

--- Invoke_Ubuntu_WinDC.ps1
+++ Invoke_Ubuntu_WinDC.ps1
@@ -1,9 +1,10 @@
 #This is a script to add a Ubuntu server to a Windows Domain
 
 $server = "10.14.2.211"
-
+$username = "jonatan"
 
 $scriptblock = {
+    try{
 #Installs the required packages
     sudo apt update
     sudo apt install realmd sssd adcli krb5-user samba-common packagekit
@@ -26,5 +27,7 @@ $scriptblock = {
         " > /etc/krb5.conf
 #Joins the domain
     sudo realm join --user=Administrator jens.jon
+    return "YES"
+    }catch{return $_Exception.Message}    
 }
-Invoke-Command -HostName $server -ScriptBlock $scriptblock -Credential (Get-Credential) -Authentication Basic -Port 22
+Invoke-Command -HostName $server -UserName $username -ScriptBlock $scriptblock -SSHTransport
